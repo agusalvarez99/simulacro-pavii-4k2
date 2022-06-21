26dd33e86b58285b7557e65bfe024a1a6f2214d0
agusalvarez99 <agusalvarez99@hotmail.com>
2022-06-21 15:13:00 -0300

    servicio estudiantes listo

--- src/app/services/estudiantes.service.ts
+++ src/app/services/estudiantes.service.ts
@@ -1,15 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Estudiantes } from '../models/estudiantes';
 
 @Injectable({
   providedIn: 'root',
 })
-export class BarriosService {
+export class EstudiantesService {
   resourceUrl: string;
   constructor(private httpClient: HttpClient) {
-    this.resourceUrl = 'https://pavii2022.azurewebsites.net/api/barrios';
+    this.resourceUrl = 'https://pavii2022.azurewebsites.net/api/estudiantes';
   }
   get() {
     return this.httpClient.get(this.resourceUrl);
+  }
+
+  post(obj: Estudiantes) {
+    return this.httpClient.post(this.resourceUrl, obj);
   }
 }
