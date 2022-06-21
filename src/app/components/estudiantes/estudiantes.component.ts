594b4cd7bf079379a617054347bbe3c47b02a35d
agusalvarez99 <agusalvarez99@hotmail.com>
2022-06-21 15:38:38 -0300

    avance del registro

--- src/app/components/estudiantes/estudiantes.component.ts
+++ src/app/components/estudiantes/estudiantes.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Barrios } from '../../models/barrios';
 import { EstudiantesService } from '../../services/estudiantes.service';
 import { Estudiantes } from '../../models/estudiantes';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-estudiantes',
@@ -24,6 +25,25 @@ export class EstudiantesComponent implements OnInit {
 
   constructor(private estudiantesService: EstudiantesService) {}
 
+  FormRegistro = new FormGroup({
+    EstudianteID: new FormControl(0),
+    EstudianteAyN: new FormControl('', [
+      Validators.required,
+      Validators.maxLength(50),
+    ]),
+    EstudianteLegajo: new FormControl(null, [
+      Validators.required,
+      Validators.pattern('[0-9]{1,5}'),
+    ]),
+    EstudianteFechaNac: new FormControl('', [
+      Validators.required,
+      Validators.pattern(
+        '(0[1-9]|[12][0-9]|3[01])[-/](0[1-9]|1[012])[-/](19|20)[0-9]{2}'
+      ),
+    ]),
+    EstudianteRegular: new FormControl(null, [Validators.required]),
+    BarrioID: new FormControl('', [Validators.required]),
+  });
   ngOnInit() {
     this.Buscar();
   }
@@ -32,5 +52,9 @@ export class EstudiantesComponent implements OnInit {
     this.estudiantesService.get().subscribe((res: Estudiantes[]) => {
       this.Items = res;
     });
+  }
+  Agregar() {
+    this.AccionABMC = 'A';
+    this.FormRegistro.reset({ IdEstudiante: 0 });
   }
 }
